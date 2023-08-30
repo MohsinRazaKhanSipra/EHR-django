1ed7e897d6a24286c4ece50b66b47201b1fc185d
Toufeeq <toufeeqsemi111@gmail.com>
2023-08-31 00:22:57 +0500

    added signal detection for receptionist model

--- DjangoEHR/patient/models.py
+++ DjangoEHR/patient/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Receptionist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank = True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=False)
     phone_number = models.CharField(max_length=15)
+    date_of_birth = models.DateField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    hire_date = models.DateField(blank=True, null=True)
+
+class Doctor(models.Model):
+    specialization = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=20)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Patient(models.Model):
     date_of_birth = models.DateField()
     address = models.TextField()
-    hire_date = models.DateField()
-    profile_picture = models.ImageField(upload_to='media/', blank=True, null=True)
+    contact_number = models.CharField(max_length=20)
+    medical_history = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
