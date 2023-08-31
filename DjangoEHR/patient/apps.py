fa7fe0d64661308af730e6ff2b2de8520061294d
Toufeeq <toufeeqsemi111@gmail.com>
2023-08-31 15:00:16 +0500

    new sidebar navbar

--- DjangoEHR/patient/apps.py
+++ DjangoEHR/patient/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+
 class PatientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'patient'
+    
+    def ready(self):
+        import patient.signals  
